e5307c0d053c38284ab58eb13a38080383146741
Ely Michael Núñez <elymichael@gmail.com>
2021-08-22 20:37:49 -0400

    Uploading some information.

--- public/Project.js
+++ public/Project.js
@@ -1,13 +1,34 @@
 let projects = [
     {
-        name: "project one",
-        tags: "#javascript, #fullstack, #ui/ux, #backend",
+        name: "ofisalud.do",
+        tags: "#javascript, #fullstack, #angularjs, #backend, #frontend",
+        image: "http://www.sitcsrd.com/images/portfolio/portfolio-1.png",
+    },
+    {
+        name: "Ofidental.do",
+        tags: "#javascript, #fullstack, #vue, #backend, #frontend",
         image: "http://via.placeholder.com/500",
     },
     {
-        name: "project two",
-        tags: "#javascript, #fullstack",
+        name: "Almanaquerd.com",
+        tags: "#javascript, #fullstack, #vue, #frontend",
         image: "http://via.placeholder.com/500",
     },
+    {
+        name: "sitcsrd.com",
+        tags: "#javascript, #fullstack, #angularjs, #frontend",
+        image: "http://via.placeholder.com/500",
+    },
+    {
+        name: "agilerta",
+        tags: "#angularjs, #javascript, #fullstack, #backend, #frontend",
+        image: "http://www.sitcsrd.com/images/portfolio/portfolio-item-6.png"
+    },
+    {
+        name: "Regalos en madera",
+        tags: "#angularjs, #javascript, #fullstack, #backend, #frontend",
+        image: "http://www.sitcsrd.com/images/portfolio/portfolio-item-8.png"
+    }
+    
     // +8 more
 ]
